2323ce0c317ddbb9540f2e817e28f4634493efb4
mayushii21 <122178787+mayushii21@users.noreply.github.com>
2025-10-31 12:09:53 -0500

    configure CORS

---
+++ api-gateway/src/main/java/com/mayushii/api_gateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.mayushii.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        // Allow requests from the React frontend
+        corsConfig.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost.localdomain:3000"));
+        corsConfig.setMaxAge(3600L); // Cache the preflight response for 1 hour
+        corsConfig.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
+        corsConfig.addAllowedHeader("*"); // Allow all headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig); // Apply this configuration to all paths
+
+        return new CorsWebFilter(source);
+    }
+}
